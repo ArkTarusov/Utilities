acb277faf4442770618dcfdc517584739e2a3f16
Ark Tarusov <ark.tarusov@gmail.com>
2018-03-16 17:50:23 +0900

    Added support for multiple dictionaries in LocalizationManager
    https://trello.com/c/Ni6b2LpL

--- AiryCat.UtilitiesForUnity/Localization/LocalizationManager.cs
+++ AiryCat.UtilitiesForUnity/Localization/LocalizationManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace AiryCat.UtilitiesForUnity.Localization
@@ -9,8 +10,20 @@ namespace AiryCat.UtilitiesForUnity.Localization
         #region Private Variable
 
         private static Dictionary<string, string> _localizedText;
-        private const string PATH_TO_FILE_LANG = "Localization/";
         private const string MISSING_TEXT_STRING = "Localized text not found";
+
+        /*
+         * Multiple paths to files with localization
+         *  {
+         *      "Localization/ui/",
+         *      "Localization/log/",
+         *      "Localization/dialog/",
+         *  }
+         */
+        private static readonly string[] PathToFileLang =
+        {
+            "Localization/"
+        };
 
         #endregion
 
@@ -32,15 +45,15 @@ namespace AiryCat.UtilitiesForUnity.Localization
         [Serializable]
         private class LocalizationData
         {
-            public LocalizationItem[] items;
+            public LocalizationItem[] Items;
 
         }
 
         [Serializable]
         private class LocalizationItem
         {
-            public string key;
-            public string value;
+            public string Key;
+            public string Value;
         }
 
         #endregion
@@ -53,24 +66,91 @@ namespace AiryCat.UtilitiesForUnity.Localization
 
         #region Public static method
 
-        public static string GetLocalizedValue(string key, object[] args = null, TextStyle style = TextStyle.AsItIsNow)
+        public static string GetLocalizedValue(string key, TextStyle style = TextStyle.AsItIsNow, params object[] args)
         {
             if (_localizedText == null || _localizedText.Count < 2)
             {
                 LoadLocalizedText();
             }
+
             if (!_localizedText.ContainsKey(key)) return MISSING_TEXT_STRING;
+            var result = args.Length < 1 ? _localizedText[key] : string.Format(_localizedText[key], args);
 
-            string result;
-            if (args != null && args.Length > 0)
+            return FormatingText(result, style);
+        }
+
+        public static void LoadLocalizedText(string lang = "")
+        {
+            _localizedText = new Dictionary<string, string>();
+
+            if (lang.Length < 2)
             {
-                result = string.Format(_localizedText[key], args);
+                lang = Application.systemLanguage == SystemLanguage.Russian ? "Rus" : "Eng";
             }
-            else
+            foreach (var it in PathToFileLang)
             {
-                result = _localizedText[key];
+                // This variable is only needed to output the log.
+                // If this bothers you or does not require log - delete.
+                var itemCount = 0;
+                // This "if" accelerates the loading of the first or only dictionary.
+                if (_localizedText.Count < 1)
+                {
+                    foreach (var value in GetLocalization(it + lang))
+                    {
+                        _localizedText.Add(value[0], value[1]);
+                        itemCount++;
+                    }
+                }
+                else
+                {
+                    foreach (var value in GetLocalization(it + lang))
+                    {
+                        if (_localizedText.ContainsKey(value[0]))
+                        {
+                            Debug.LogWarning($"[Localization] Dublicate key found: {value[0]}");
+                            continue;
+                        }
+                        _localizedText.Add(value[0], value[1]);
+                        itemCount++;
+                    }
+                }
+
+                if (itemCount > 0)
+                {
+                    Debug.Log($"[Localization] Data loaded, dictionary {it + lang} contains: {itemCount} entries");
+                }
+            }
+            SwitchLangEvent?.Invoke();
+        }
+
+        #endregion
+
+        #region Private static method
+
+        private static List<string[]> GetLocalization(string pathFileName)
+        {
+            var result = new List<string[]>();
+
+            var assets = Resources.LoadAll<TextAsset>(pathFileName);
+            Resources.UnloadUnusedAssets();
+
+            if (assets == null || assets.Length == 0)
+            {
+                Debug.LogError($"[Localization] File: {pathFileName}  not found.");
+                return null;
             }
 
+            foreach (var data in assets)
+            {
+                var loadedData = JsonUtility.FromJson<LocalizationData>(data.text);
+                result.AddRange(loadedData.Items.Select(t => new[] { t.Key, t.Value }));
+            }
+            return result;
+        }
+
+        private static string FormatingText(string text, TextStyle style)
+        {
+            var result = text;
             switch (style)
             {
                 case TextStyle.AsItIsNow:
@@ -90,64 +170,6 @@ namespace AiryCat.UtilitiesForUnity.Localization
             }
             return result;
         }
-
-        public static void LoadLocalizedText(string fileName = "")
-        {
-            if (fileName.Length < 2)
-            {
-                fileName = Application.systemLanguage == SystemLanguage.Russian ? "Rus" : "Eng";
-            }
-            _localizedText = GetLocalization(fileName);
-
-            if (_localizedText != null && _localizedText.Count > 1)
-            {
-                Debug.Log("[Localization] Data loaded, dictionary contains: " + _localizedText.Count + " entries");
-                SwitchLangEvent?.Invoke();
-            }
-            else
-            {
-                Debug.LogError("[Localization] Cannot find file!");
-            }
-
-        }
-
-        #endregion
-
-        #region Private static method
-
-        private static Dictionary<string, string> GetLocalization(string fileName)
-        {
-            var result = new Dictionary<string, string>();
-
-            var assets = Resources.LoadAll<TextAsset>(PATH_TO_FILE_LANG + fileName);
-            Resources.UnloadUnusedAssets();
-
-            if (assets == null || assets.Length == 0)
-            {
-                Debug.LogError($"[Localization] File: {PATH_TO_FILE_LANG + fileName}  not found.");
-                return null;
-            }
-
-            foreach (var data in assets)
-            {
-                Debug.Log(data);
-                var loadedData = JsonUtility.FromJson<LocalizationData>(data.text);
-                Debug.Log(loadedData.ToString());
-                Debug.Log(loadedData.items.Length);
-                for (int i = 0; i < loadedData.items.Length; i++)
-                {
-                    if (result.ContainsKey(loadedData.items[i].key))
-                    {
-                        Debug.LogWarning($"[Localization] Duplicate found: {loadedData.items[i].key}");
-                        continue;
-                    }
-                    result.Add(loadedData.items[i].key, loadedData.items[i].value);
-                }
-
-            }
-            return result;
-        }
-
         #endregion
     }
 }
